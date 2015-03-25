315dd84cc5ea73a9d916c106b1ccb83474ab151f
Mark Boas <markb@happyworm.com>
2015-03-25 21:42:30 +0100

    tweaks

--- hyperaudio-lite.js
+++ hyperaudio-lite.js
@@ -3,14 +3,16 @@ var hyperaudiolite = (function () {
   var hal = {}, 
     transcript, 
     words, 
-    player;
+    player,
+    paraIndex;
 
   function init() {
     words = transcript.getElementsByTagName('a');
     paras = transcript.getElementsByTagName('p');
     player = document.getElementById("hyperplayer");
-    words[0].className = "active";
-    paras[0].className = "active";
+    paraIndex = 0;
+    words[0].classList.add("active");
+    paras[0].classList.add("active");
     transcript.addEventListener("click", setPlayHead, false);
     player.addEventListener("timeupdate", checkPlayHead, false);
   }
@@ -24,28 +26,59 @@ var hyperaudiolite = (function () {
   }
 
   function checkPlayHead(e) {
-    var activewords = transcript.getElementsByClassName('active');
-    var activeparas = transcript.getElementsByClassName('active');
+    var activeitems = transcript.getElementsByClassName('active');
+    var activeitemsLength = activeitems.length;
 
-    if (activewords) {
-      for (a = 0; a < activewords.length; a++) {
-        activewords[a].className = "";
+    for (var a = 0; a < activeitemsLength; a++) {
+      if (activeitems[a]) { // TODO: look into why we need this
+        activeitems[a].classList.remove("active");
       }
     }
 
-    if (activeparas) {
-      for (a = 0; a < activeparas.length; a++) {
-        activeparas[a].className = "";
-      }
-    }
+    // Establish current paragraph index
 
-    for (i = 0; i < words.length; i++) {
-      if (parseInt(words[i].getAttribute("data-m"))/1000 >= player.currentTime) {
-        words[i].setAttribute("class", "active");
-        words[i].parentNode.setAttribute("class", "active");
+    var currentParaIndex;
+
+    for (i = 1; i < words.length; i++) {
+      if (parseInt(words[i].getAttribute("data-m"))/1000 > player.currentTime) {
+
+        // TODO: look for a better way of doing this
+        var strayActive = transcript.getElementsByClassName('active')[0];
+        strayActive.classList.remove("active");
+
+        // word time is in the future - set the previous word as active.
+        words[i-1].classList.add("active");
+        words[i-1].parentNode.classList.add("active");
+
+        paras = transcript.getElementsByTagName('p');
+
+        for (a = 0; a < paras.length; a++) {
+
+          if (paras[a].classList.contains("active")) {
+            currentParaIndex = a;
+            break;
+          }
+        }
+
+        if (currentParaIndex != paraIndex) {
+
+          Velocity(words[i].parentNode, "scroll", { 
+            /*container: transcript,*/
+            duration: 800,
+            delay: 0
+          });
+
+          paraIndex = currentParaIndex;
+        }
+
         break;
       }
     }
+  }
+
+  hal.init = function() {
+    transcript = document.getElementById("hypertranscript");
+    init();
   }
 
   hal.loadTranscript = function(url) {
