cda8e86a8b7f9cf8687e51db6a6f03d8be34a77c
Mark Boas <markb@happyworm.com>
2021-10-20 13:05:44 +0200

    youtube example and youtube fix

--- js/hyperaudio-lite.js
+++ js/hyperaudio-lite.js
@@ -78,7 +78,7 @@ class HyperaudioLite {
       const firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-      window.onYouTubeIframeAPIReady = function () {
+      window.onYouTubeIframeAPIReady = () => {
         this.player = new YT.Player(mediaElementId, {
           events: {
             onStateChange: onPlayerStateChange,
@@ -86,7 +86,7 @@ class HyperaudioLite {
         });
       };
 
-      onPlayerStateChange = event => {
+      let onPlayerStateChange = event => {
         if (event.data === 1) {
           // playing
           this.checkPlayHead();
@@ -135,7 +135,7 @@ class HyperaudioLite {
       } else {
         // Assume YouTube
 
-        window.onYouTubeIframeAPIReady = function () {
+        window.onYouTubeIframeAPIReady = () => {
           this.player = new YT.Player(mediaElementId, {
             playerVars: { autoplay: 1 },
             events: {
