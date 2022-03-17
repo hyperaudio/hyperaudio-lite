27f17b2645a7b311bb7bf40a808db2d43de81d26
Mark Boas <markb@happyworm.com>
2022-03-17 12:23:18 +0100

    90 allow multiple youtube players

--- js/hyperaudio-lite.js
+++ js/hyperaudio-lite.js
@@ -84,7 +84,13 @@ class HyperaudioLite {
       const firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+      const previousYTEvent = window.onYouTubeIframeAPIReady;
       window.onYouTubeIframeAPIReady = () => {
+
+        if (typeof previousYTEvent !== 'undefined') { // used for multiple YouTube players
+          previousYTEvent();
+        }
+        
         this.player = new YT.Player(mediaElementId, {
           events: {
             onStateChange: onPlayerStateChange,
