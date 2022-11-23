34f67f1d0ad23898bc6f8e1d28b93ba24b24691b
Mark Boas <markb@happyworm.com>
2022-11-23 17:39:39 +0100

    videojs integration and example #130

--- js/hyperaudio-lite.js
+++ js/hyperaudio-lite.js
@@ -53,9 +53,32 @@ function soundcloudPlayer(instance) {
   }
 }
 
-function vimeoPlayer(instance) {
+function videojsPlayer(instance) {
   const tag = document.createElement('script');
 
+  this.player = videojs.getPlayer(instance.player.id);
+
+  this.getTime = () => {
+    return new Promise((resolve) => {
+      resolve(this.player.currentTime());
+    });
+  }
+
+  this.setTime = (seconds) => {
+    this.player.currentTime(seconds);
+  }
+
+  this.play = () => {
+    this.player.play();
+  }
+
+  this.pause = () => {
+    this.player.pause();
+  }
+}
+
+function vimeoPlayer(instance) {
+  const tag = document.createElement('script');
 
   const iframe = document.querySelector('iframe');
     this.player = new Vimeo.Player(iframe);
@@ -135,6 +158,7 @@ const hyperaudioPlayerOptions = {
   "native": nativePlayer,
   "soundcloud": soundcloudPlayer,
   "youtube": youtubePlayer,
+  "videojs": videojsPlayer,
   "vimeo": vimeoPlayer
 }
 
